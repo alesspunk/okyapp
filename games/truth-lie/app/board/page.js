b744fe303d957c55e32a3528c40c75050aa0750d
Alessandro Perez <alessandroperez@Mac.ht.home>
2026-03-02 13:39:04 -0500

    Remove games/truth-lie folder

--- games/truth-lie/app/board/page.js
+++
@@ -1,9 +0,0 @@
-import PublicBoard from "../../components/PublicBoard";
-
-export default function BoardPage() {
-  return (
-    <main className="tl-shell tl-stack-lg">
-      <PublicBoard />
-    </main>
-  );
-}
