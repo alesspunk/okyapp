16f8a8a218b0c8e8bf90107afc9ff50df4cf86e8
Alessandro Perez <alessandroperez@Mac.ht.home>
2026-03-09 10:23:23 -0400

    fix: force font awesome svg replacement in storybook and mockups

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,17 @@
 import "../stories/mars.css";
 
 const preview = {
+  decorators: [
+    (story) => {
+      const rendered = story();
+      requestAnimationFrame(() => {
+        if (window.FontAwesome?.dom?.i2svg) {
+          window.FontAwesome.dom.i2svg({ node: document.body });
+        }
+      });
+      return rendered;
+    },
+  ],
   parameters: {
     layout: "centered",
     controls: { expanded: true, matchers: { color: /(background|color)$/i } },
